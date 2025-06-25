2d60e0c5eecd750f29d5b48fa37f18cefcc873c5
Gilles FRANCOIS <gilles@lesfrancois.com>
2025-06-25 08:34:46 +0200

    Allow routes to specify their own base path

--- routes/personne.js
+++ routes/personne.js
@@ -2,6 +2,8 @@ import express from 'express';
 import { getPersonne, getPersonnes, updatePersonne } from '../inc/personnes.js';
 import { handleResponse } from '../inc/response.js';
 const router = express.Router();
+// Base path for this router
+export const routePath = '/personne';
 
 router.get('/', handleResponse(async (req, res) => {
     const personne = await getPersonne({ id: req.user.personne_id });
